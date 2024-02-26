6be459e6e4de70f363be86b83f7e35bd6e8dc026
alx <world.zay@gmail.com>
2024-02-26 16:36:39 +0300

    Home controller bugfix

--- app/Http/Controller/Home.php
+++ app/Http/Controller/Home.php
@@ -2,20 +2,29 @@
 
 namespace App\Http\Controller;
 
-use App\Model\ModelHome;
+
 use Sys\Controller\WebController;
+use Sys\Create\ModelCreateDB;
 use Az\Route\Route;
+use Symfony\Component\Yaml\Yaml;
 
 final class Home extends WebController 
 {
     // #[Route(ajax: true)]
     // #[Route(methods: ['post', 'put'])]
-    public function __invoke(ModelHome $model)
+    public function __invoke()
     {
+        $connect = env('connect.mysql');
+        $config = Yaml::parseFile('../' . ROOTPATH . 'docker-compose.yml');
+        $root_password = $config['services']['mysql']['environment']['MYSQL_ROOT_PASSWORD'];
+        $dbname = (empty($dbname)) ? $connect['database'] : $dbname;
+        $host = $connect['host'];
+
+        $model = new ModelCreateDB($host, $root_password);
         // 1/0;
         $data = [
-            'h1' => 'Welcome!',
-            'isdb' => $model->dbExits(),
+            'h1' => 'Welcome!!',
+            'isdb' => $model->dbExists($dbname),
         ];
 
         return $this->render('home', $data);
