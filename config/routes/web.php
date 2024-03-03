5d3ca2905f69382b392ee790ce69e530373a66ab
alx <world.zay@gmail.com>
2024-03-03 13:52:51 +0300

    modified: config/routes/web.php

--- config/routes/web.php
+++ config/routes/web.php
@@ -9,14 +9,14 @@ use Sys\Middleware\ControllerAttributeMiddleware;
 use Sys\Profiler\Profiler;
 use Sys\Profiler\Controller as ProfilerController;
 
-if (in_array(Profiler::class, config('post_process', null, null, false))) {
-    $this->route->get('/~profiler/{uri?}', ProfilerController::class)
-        ->tokens(['uri' => '.*']);
-}
+// if (in_array(Profiler::class, config('post_process', null, null, false))) {
+//     $this->route->get('/~profiler/{uri?}', ProfilerController::class)
+//         ->tokens(['uri' => '.*']);
+// }
 
 require_once APPPATH . 'modules/Guide/routes.php';
-require_once APPPATH . 'auth/config/routes.php';
+// require_once APPPATH . 'auth/config/routes.php';
 
 $this->route->controller('{action?}/{param?}', Home::class, 'home')
-    ->pipe(AuthMiddleware::class)
+    // ->pipe(AuthMiddleware::class)
     ;
